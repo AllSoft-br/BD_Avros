1949850a4cbd3371cb60fadf15514cd8035b73af
Wallace Oliveira <wallace.ol@outlook.com>
2015-05-12 12:38:22 -0300

    Criando procedures de exclusão

--- Banco/EST_Inserts.sql
+++ Banco/EST_Inserts.sql
@@ -1,6 +1,7 @@
 #INSERTS DAS TABELAS
 
-#---------------------------------------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 #INSERTS DA TABELA LOGIN
 INSERT INTO tbl_login (nome, CPF, login, senha, admin)
 VALUES ('Wallace Abreu de Oliveira', '53141191913', 'admin', '123', 1);
@@ -8,90 +9,144 @@ VALUES ('Wallace Abreu de Oliveira', '53141191913', 'admin', '123', 1);
 INSERT INTO tbl_login (nome, CPF, login, senha)
 VALUES ('Eduardo de Souza', '12398765436', 'Edu', '321');
 
+INSERT INTO tbl_login (nome, CPF, login, senha)
+VALUES ('Lohan William', '63826344802', 'Lohan', '231');
+
+INSERT INTO tbl_login (nome, CPF, login, senha)
+VALUES ('Ronaldo Nazaro', '73648371758', 'Rnd', '213');
+
 SELECT * FROM tbl_login;
-#---------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-#---------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 #INSERTS DA TABELA CLIENTE
 INSERT INTO tbl_cliente (nome, CPF, data_nasc, tel, fk_id_login)
 VALUES ('Douglas Lambertinny', '64837748274', '1994/04/20', '9876-5432', 1);
 
 INSERT INTO tbl_cliente (nome, CPF, sexo, data_nasc, tel, fk_id_login)
-VALUES ('Luana Ferreira Nascimento', '44939416852', 1, '1997-07-14', '1234-5678', 2);
+VALUES ('Luana Ferreira Nascimento', '37485574758', 1, '1997-07-14', '1234-5678', 2);
+
+INSERT INTO tbl_cliente (nome, CPF, data_nasc, tel, fk_id_login)
+VALUES ('Japa Itapecerica', '63857365982', '1998-03-19', '8374-8374', 2);
+
+INSERT INTO tbl_cliente (nome, CPF, sexo, data_nasc, tel, fk_id_login)
+VALUES ('Vanessa Lira', '83647362717', 1, '1992-01-07', '5601-0893', 3);
 
 SELECT * FROM tbl_cliente;
-#------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-#------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 #TABELA REPRESENTANTE
 INSERT INTO tbl_representante (nome, CPF, sexo,  data_nasc, tel)
 VALUES ('Eucrimaria Luiza Ferreira Nascimento', '37474456283', 1, '1960/12/10', '6253-7474'); 
 
+INSERT INTO tbl_representante (nome, CPF, data_nasc, tel)
+VALUES ('China da Serra', '93472647364', '1978/02/01', '6208-8062'); 
+
 SELECT * FROM tbl_representante;
-#------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-#------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 #TABELA PARENTESCO
-INSERT INTO tbl_parentesco (tipo_parentesco)
-VALUES ('AVÔ');
-
 INSERT INTO tbl_parentesco (tipo_parentesco)
 VALUES ('AVÓ');
 
 INSERT INTO tbl_parentesco (tipo_parentesco)
-VALUES ('MADRASTA');
+VALUES ('AVÔ');
 
 INSERT INTO tbl_parentesco (tipo_parentesco)
 VALUES ('TIA');
 
 INSERT INTO tbl_parentesco (tipo_parentesco)
-VALUES ('MÃE');
+VALUES ('TIO');
 
 INSERT INTO tbl_parentesco (tipo_parentesco)
-VALUES ('PADRASTO');
+VALUES ('MÃE');
 
 INSERT INTO tbl_parentesco (tipo_parentesco)
 VALUES ('PAI');
 
+INSERT INTO tbl_parentesco (tipo_parentesco)
+VALUES ('MADRASTA');
+
+INSERT INTO tbl_parentesco (tipo_parentesco)
+VALUES ('PADRASTO');
+
 SELECT * FROM tbl_parentesco;
-#------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-#------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 #TABELA RELAÇÃO
 INSERT INTO tbl_rel (fk_id_cli, fk_id_parentesco, fk_id_representante)
-VALUES (1, 2, 1);
+VALUES (2, 2, 1);
+
+INSERT INTO tbl_rel (fk_id_cli, fk_id_parentesco, fk_id_representante)
+VALUES (3, 1, 2);
 
 SELECT * FROM tbl_rel;
-#------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-#------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 #TABELA ORCAMENTO
 insert into tbl_orcamento(tipo_pagamento, valor_total, qntd_sessao, fk_id_cli_orc)
-values ('cartão', 1090.98, 2, 1);
+values ('cartão', 1090.98, 4, 1);
 
-select * from tbl_orcamento where cod_orc = 1004;
-#------------------------------------------------------------------------------------------------------------
+insert into tbl_orcamento(tipo_pagamento, valor_total, qntd_sessao, fk_id_cli_orc)
+values ('Dinheiro', 398.80, 3, 2);
+
+insert into tbl_orcamento(tipo_pagamento, valor_total, qntd_sessao, fk_id_cli_orc)
+values ('Cartão', 200.00, 2, 3);
+
+insert into tbl_orcamento(tipo_pagamento, valor_total, qntd_sessao, fk_id_cli_orc)
+values ('Não específicado ..hmm..', 50.00, 1, 4);
+
+select * from tbl_orcamento;
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-#------------------------------------------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 #TABELA SESSÃO
-INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, desconto, fk_cod_orc)
-VALUES (1, 100.00, 'cartão', '2015-06-15', '15:00', 0, 1004);
+INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, fk_cod_orc)
+VALUES ('0', 100.00, 'cartão', '2015-06-15', '15:00', 1);
 
-INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, desconto, fk_cod_orc)
-VALUES ('0', 100.00, 'cartão', '2015-06-25', '13:00', 0, 1);
+INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, fk_cod_orc)
+VALUES ('0', 100.00, 'cartão', '2015-06-25', '13:00', 1);
 
-INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, desconto, fk_cod_orc)
-VALUES ('0', 100.00, 'cartão', '2015-07-10', '15:00', 0, 1);
+INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, fk_cod_orc)
+VALUES ('0', 100.00, 'cartão', '2015-07-10', '15:00', 1);
 
-INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, desconto, fk_cod_orc)
-VALUES ('1', 100.00, 'cartão', '2015-07-30', '15:00', 0, 1);
+INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, fk_cod_orc)
+VALUES ('1', 100.00, 'cartão', '2015-07-30', '15:00', 1);
+
+
+
+INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, fk_cod_orc)
+VALUES ('0', 132.93, 'Dinheiro', '2015-06-22', '19:00', 2);
+
+INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, fk_cod_orc)
+VALUES ('0', 132.93, 'Dinheiro', '2015-07-07', '19:00', 2);
+
+INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, fk_cod_orc)
+VALUES ('1', 132.93, 'Dinheiro', '2015-07-22', '19:00', 2);
+
+
+
+INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, fk_cod_orc)
+VALUES ('0', 100.00, 'Cartão', '2015-08-12', '17:00', 3);
+
+INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, fk_cod_orc)
+VALUES ('1', 100.00, 'Cartão', '2015-08-30', '12:00', 3);
+
+
+
+INSERT INTO tbl_sessao(concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, fk_cod_orc)
+VALUES ('1', 50.00, 'Não especificada ..hm..', '2015-08-15', '22:00', 4);
 
 SELECT * FROM tbl_sessao;
-#------------------------------------------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------------------------------------------------------------------------
--- Banco/EST_Procedure.sql
+++ Banco/EST_Procedure.sql
@@ -1,45 +1,77 @@
 #A procedure será chamada quando todas as sessões forem concluidas
-#Assim ele cria uma cópia dos dados para as respectivas tablas de orçamento e sessão concluidas
+#Assim ele cria uma cópia dos dados para as respectivas tabelas de orçamento e sessões concluidas
 
 DELIMITER $
-CREATE PROCEDURE orcamento_concluido(IN id_orc INT(10))
+CREATE PROCEDURE orcamento_concluido(IN X INT(10))
 
 	BEGIN
-		DECLARE nsessoes int(10);
-		DECLARE concluidas int(10);
-		
-		select qntd_sessao from tbl_orcamento where cod_orc = id_orc into @nsessoes;
-		select count(id_sessao) from tbl_sessao 
-				where concluida = 1
-				and fk_cod_orc = id_orc into @concluidas;
 
-		if @nsessoes <= @concluidas then
+		INSERT INTO tbl_orccon(cod_orccon, criado_em, tipo_pagamento, valor_total, qntd_sessao, fk_id_cli_orccon)
+		SELECT cod_orc, criado_em, tipo_pagamento, valor_total, qntd_sessao, fk_id_cli_orc
+		FROM tbl_orcamento
+		WHERE cod_orc = X;
 
-			INSERT INTO tbl_orccon(cod_orccon, criado_em, tipo_pagamento, valor_total, qntd_sessao, fk_id_cli_orccon)
-			SELECT cod_orc, criado_em, tipo_pagamento, valor_total, qntd_sessao, fk_id_cli_orc
-			FROM tbl_orcamento
-			WHERE cod_orc = id_orc;
+		INSERT INTO tbl_sescon(id_sescon, concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, desconto, fk_cod_orccon)
+		SELECT id_sessao, concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, desconto, fk_cod_orc
+		FROM tbl_sessao	
+		WHERE fk_cod_orc = X
+		#GROUP BY fk_cod_orc
+		;
 
-			INSERT INTO tbl_sescon(id_sescon, concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, desconto, fk_cod_orccon)
-			SELECT id_sessao, concluida, valor_sessao, tipo_pagamento, data_agendada, hora_agendada, desconto, fk_cod_orc
-			FROM tbl_sessao	
-			WHERE fk_cod_orc = id_orc
-			#GROUP BY fk_cod_orc
-			;
-
-			DELETE FROM tbl_sessao WHERE fk_cod_orc = id_orc;
-			DELETE FROM tbl_orcamento WHERE cod_orc = id_orc;
-
-		end if;
+		DELETE FROM tbl_sessao WHERE fk_cod_orc = x;
+		DELETE FROM tbl_orcamento WHERE cod_orc = x;
 	
 	END $
 
 Delimiter ;
 
-
-CALL orcamento_concluido(1004);
+CALL orcamento_concluido(1);
 
 SELECT * FROM tbl_orcamento;
 SELECT * FROM tbl_orccon;
 SELECT * FROM tbl_sessao;
 SELECT * FROM tbl_sescon;
+#----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------
+#Procedure que deleta as sessões
+
+DELIMITER $
+CREATE PROCEDURE del_sessao(IN fk_id_orc INT(10))
+
+	BEGIN
+
+		DELETE FROM tbl_sessao WHERE fk_cod_orc = fk_id_orc;
+	
+	END $
+
+Delimiter ;
+
+CALL del_sessao(2);
+
+SELECT * FROM tbl_sessao;
+SELECT * FROM tbl_orcamento;
+#------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------
+#Procedure que deleta os orçamentos
+
+DELIMITER $
+CREATE PROCEDURE del_orcamento(IN fk_id_orc INT(10))
+
+	BEGIN
+
+		DELETE FROM tbl_orcamento WHERE cod_orc = fk_id_orc;
+	
+	END $
+
+Delimiter ;
+
+CALL del_orcamento(2);
+
+SELECT * FROM tbl_sessao;
+SELECT * FROM tbl_orcamento;
+#------------------------------------------------------------------------------------------------------------------------------------------------
+
